dd03c56fe80438fa8db3a4e49f71e3942b31fc21
fatusia <fatboc@gmail.com>
2020-12-02 14:19:59 +0100

    v2.63

--- php-docker/basic/config/db.php
+++ php-docker/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=172.17.0.8;port=3306;dbname=studia',
+    'dsn' => 'mysql:host=mysql-service;port=3306;dbname=studia',
     'username' => 'bui2020',
     'password' => 'BUI-zimowy-2020',
     'charset' => 'utf8',
