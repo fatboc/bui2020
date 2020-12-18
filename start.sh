b0021d5adde3b5e1c9f585efaab0a0d5374ca074
fatusia <fatboc@gmail.com>
2020-12-18 19:43:00 +0100

    start

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 kubectl delete -f jaeger/jaeger.yaml
-kubectl delete -f mysql/mysql-pv.yaml
 kubectl delete -f mysql/mysql.yaml
 kubectl delete -f python/python.yaml
 
 kubectl create -f jaeger/jaeger.yaml
-kubectl create -f mysql/mysql-pv.yaml
 kubectl create -f mysql/mysql.yaml
 kubectl create -f python/python.yaml
