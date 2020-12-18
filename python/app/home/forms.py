1139c203ddba41be8ddfc98dd00235d01cfde36f
fatusia <fatboc@gmail.com>
2020-12-18 19:27:27 +0100

    format daty

--- python/app/home/forms.py
+++ python/app/home/forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 class ZadanieForm(FlaskForm):
     nr_kursu = IntegerField('Numer kursu', validators=[DataRequired()])
     typ = StringField('Rodzaj zadania', validators=[DataRequired()])
-    termin = DateField('Termin', format='%Y-%m-%d', validators=[DataRequired()])
+    termin = DateField('Termin (format: YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
     opis = StringField('Opis zadania')
     submit = SubmitField('Zatwierdź')
     
